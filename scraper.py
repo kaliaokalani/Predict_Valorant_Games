229c399fb5ed205fe8231ead9e4970d4ea1cece3
kaliaokalani <kaliaokalani11@gmail.com>
2024-04-23 12:15:10 -0700

    fixed the code for finding the team_names

--- scraper.py
+++ scraper.py
@@ -3,24 +3,7 @@ import requests as requests
 from bs4 import BeautifulSoup
 
 urls = [
-    "https://www.vlr.gg/297256/t1-vs-paper-rex-champions-tour-2024-pacific-kickoff-playoffs-sf",
-    "https://www.vlr.gg/297257/drx-vs-gen-g-champions-tour-2024-pacific-kickoff-playoffs-sf",
-    "https://www.vlr.gg/297258/paper-rex-vs-gen-g-champions-tour-2024-pacific-kickoff-playoffs-gf",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf",
-    "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf",
-    "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf",
-    "https://www.vlr.gg/303093/team-heretics-vs-natus-vincere-champions-tour-2024-emea-kickoff-playoffs-sf",
-    "https://www.vlr.gg/303094/fnatic-vs-karmine-corp-champions-tour-2024-emea-kickoff-playoffs-sf",
-    "https://www.vlr.gg/303095/team-heretics-vs-karmine-corp-champions-tour-2024-emea-kickoff-playoffs-gf",
-    "https://www.vlr.gg/298833/funplus-phoenix-vs-dragon-ranger-gaming-champions-tour-2024-china-kickoff-sf",
-    "https://www.vlr.gg/298834/edward-gaming-vs-trace-esports-champions-tour-2024-china-kickoff-sf",
-    "https://www.vlr.gg/298835/funplus-phoenix-vs-edward-gaming-champions-tour-2024-china-kickoff-gf",
-    "https://www.vlr.gg/312777/gen-g-vs-paper-rex-champions-tour-2024-masters-madrid-ubsf",
-    "https://www.vlr.gg/312776/sentinels-vs-loud-champions-tour-2024-masters-madrid-ubsf",
-    "https://www.vlr.gg/312778/gen-g-vs-sentinels-champions-tour-2024-masters-madrid-ubf",
-    "https://www.vlr.gg/312780/paper-rex-vs-loud-champions-tour-2024-masters-madrid-lr1",
-    "https://www.vlr.gg/312781/sentinels-vs-paper-rex-champions-tour-2024-masters-madrid-lbf",
-    "https://www.vlr.gg/312779/gen-g-vs-sentinels-champions-tour-2024-masters-madrid-gf"
+    "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160718&tab=overview"
 ]
 
 # All data collected
@@ -33,14 +16,13 @@ for url in urls:
 
         # Extract data from the current URL
         
-        team_names = [team.text.strip() for team in soup.find_all('div', class_='team')]
-        agents_played = [agent.text.strip() for agent in soup.find_all('div', class_='col-md-5')]
+        team_names = [team.text.strip() for team in soup.find_all('div', class_='wf-title-med')]
+        agents_played = [agent.text.strip() for agent in soup.find_all('div', class_='vm-stats-game')]
         map_results = [result.text.strip() for result in soup.find_all('div', class_='map-score')]
         economy_ratings = [rating.text.strip() for rating in soup.find_all('div', class_='economy-score')]
 
         # Create a dictionary to store all the data
         url_data = {
-            "URL" : url,
             "Team Names": team_names,
             "Agents Played": agents_played,
             "Map Results": map_results,
