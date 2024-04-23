85713963819832535a3a3ee1981dda0590cd6a42
kaliaokalani <kaliaokalani11@gmail.com>
2024-04-23 16:13:39 -0700

    fixed the code to have a column of the score

--- scraper.py
+++ scraper.py
@@ -4,8 +4,16 @@ from bs4 import BeautifulSoup
 
 urls = [
     "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160718&tab=overview",
+    "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160719&tab=overview",
+    "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160720&tab=overview",
     "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf/?game=160721&tab=overview",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160715&tab=overview"
+    "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf/?game=160722&tab=overview",
+    "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf/?game=160723&tab=overview",
+    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160715&tab=overview",
+    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160716&tab=overview",
+    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160717&tab=overview",
+    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160724&tab=overview",
+    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160725&tab=overview"
 ]
 
 # All data collected
@@ -22,7 +30,7 @@ for url in urls:
         map_name = [map.text.strip().split('\t')[0] for map in soup.find_all('div', class_='map') [0]]
         agent_names = [a.find('img').get('alt') for a in soup.find_all("span", {"class": "stats-sq mod-agent small"}) if a.find('img') ]
 
-        map_results = [result.text.strip() for result in soup.find_all('div', class_='map-score')]
+        map_results = [result.text.strip().split('\t')[0] for result in soup.find_all('div', class_='score')]
         economy_ratings = [rating.text.strip() for rating in soup.find_all('div', class_='economy-score')]
 
 
