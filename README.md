9c804355f7f439028287ebb7e643e21f3c327b9a
kaliaokalani <kaliaokalani11@gmail.com>
2024-04-25 11:37:06 -0700

    added the csv file and README file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Predict_Valorant_Games
+
+## Valorant Agent and Map Analysis with Python
+
+## Exploring Valorant Data
+
+## Data Analysis
+
+## Insights
+
+## Strategies
+
+## Conclusions
--- scraper.py
+++ scraper.py
@@ -5,16 +5,8 @@ import csv
 
 urls = [
     "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160718&tab=overview",
-    "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160719&tab=overview",
-    "https://www.vlr.gg/303087/evil-geniuses-vs-loud-champions-tour-2024-americas-kickoff-sf/?game=160720&tab=overview",
     "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf/?game=160721&tab=overview",
-    "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf/?game=160722&tab=overview",
-    "https://www.vlr.gg/303088/nrg-esports-vs-sentinels-champions-tour-2024-americas-kickoff-sf/?game=160723&tab=overview",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160715&tab=overview",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160716&tab=overview",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160717&tab=overview",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160724&tab=overview",
-    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160725&tab=overview"
+    "https://www.vlr.gg/303086/loud-vs-sentinels-champions-tour-2024-americas-kickoff-gf/?game=160715&tab=overview"
 ]
 
 # All data collected
@@ -33,10 +25,20 @@ for url in urls:
         map_results = [result.text.strip().split('\t')[0] for result in soup.find('div', class_='score')]
         economy_ratings = [rating.text.strip() for rating in soup.find_all('div', class_='wf-table-inset mod-econ')]
 
+        # # Group the agents by team in groups of 5
+        # group_agents = [agent_names[i:i+5] for i in range (0, len(agent_names), 5)]
+
+        # team_map_winner = [mapwin.find('div').get('team-name') for mapwin in soup.find_all("div", {"class": "team"}) if mapwin.find('div')] 
+
+        team_map_winner = []
+        map_winners = soup.find_all("div", {"class": "score mod-win"})
+
         # Create a list to store all the data
         url_data = [
             team_names,
             map_name,
+            # group_agents,
+            team_map_winner,
             agent_names,
             map_results,
             economy_ratings
@@ -50,6 +52,6 @@ for url in urls:
 # Save Data to a CSV File
 with open ('data.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['Team Names', 'Map Played', 'Agents Played', 'Map Results', 'Economy Rating'])
+    writer.writerow(['Team Names', 'Map Played', 'Map Winner', 'Agents Played', 'Map Results', 'Economy Rating'])
     for data_row in all_data:
         writer.writerow(data_row)
